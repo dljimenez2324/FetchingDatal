9f7ca4d8d1afd0ca4c9dea656da217c831cef095
Jose Luis Martinez Lopez <josemartinez8282@gmail.com>
2024-06-05 20:15:39 -0700

    Fetching with Axios

---
+++ src/components/UseEffectExample2.tsx
@@ -0,0 +1,35 @@
+import { useEffect, useRef } from "react"
+
+
+const UseEffectExample2 = () => {
+
+    const ref = useRef<HTMLInputElement>(null);
+    
+    
+    
+    useEffect(() => {
+        
+        if(ref.current) ref.current.focus()
+    
+      
+    }, [])
+
+    useEffect(() => {
+    document.title = 'This is cool'
+    
+      
+    }, [])
+    
+
+
+    
+
+  return (
+    <>
+        <h1 className="text-center">UseEffect Example 2</h1>
+        <input ref={ref} type="text" className="form-control" />
+    </>
+  )
+}
+
+export default UseEffectExample2
