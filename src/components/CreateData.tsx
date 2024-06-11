03d6c936add2b496f74ebe6156af829c1d925ae4
Jose Luis Martinez Lopez <josemartinez8282@gmail.com>
2024-06-10 19:46:19 -0700

    Update Data, Create Data, Started Services

---
+++ src/components/CreateData.tsx
@@ -0,0 +1,86 @@
+
+import { useEffect, useState } from "react";
+import apiClient from "../services/apiClient";
+
+interface User {
+  id: number;
+  name: string;
+
+}
+
+const CreateData = () => {
+  //we need a useState to help us hold the state of our users
+  const [users, setUsers] = useState<User[]>([]);
+  const [error, setError] = useState("");
+  //UseState for our isLoading indicator
+  const [isLoading, setIsLoading] = useState(false);
+
+  ///Create a function to helps us fetch our data with axios
+  const FetchData = () => {
+    setIsLoading(true);
+    apiClient
+      .get("https://jsonplaceholder.typicode.com/users")
+      .then((response) => {
+        setUsers(response.data);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        setError(error.message);
+        setIsLoading(false);
+      });
+  };
+
+  //UseEffect to help us with our FetchingData
+
+  useEffect(() => {
+    FetchData();
+  }, []);
+
+  ///Lets create a helper function to help us Create our user
+  const addUser = () => {
+    //original users []
+    const originalUsers = [...users]
+    //wer are going to have a new object with id and name
+    const newUser = {id: 0, name: 'Aaron'};
+    //set our users and spread all users and add our new user
+    setUsers([newUser,...users])
+    //we need to send this updated data to our back-end
+    apiClient.post('https://jsonplaceholder.typicode.com/users',newUser)
+    .then(response => setUsers([response.data,...users]))
+    .catch(error => {
+      setError(error.message);
+      setUsers(originalUsers);
+
+    })
+    
+  }
+  
+
+  return (
+    <>
+      <h1 className="text-center">CRUD Create with apiClient</h1>
+      <button className="btn btn-outline-primary mx-3 mb-3" onClick={addUser}>Add</button>
+      <ul className="list-group">
+        {users.map((user) => (
+          <li
+            className="list-group-item d-flex justify-content-between"
+            key={user.id}
+          >
+            {user.name}
+            <button
+              
+              className="btn btn-outline-danger"
+            >
+              Delete
+            </button>{" "}
+          </li>
+        ))}
+
+        {error && <p className="text-danger">{error}</p>}
+        {isLoading && <div className="spinner-border"></div>}
+      </ul>
+    </>
+  );
+};
+
+export default CreateData;
